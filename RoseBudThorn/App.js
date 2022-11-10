1e6c0832f7e55d7318d14e245dfaab3d55e261a1
emmettbarry <emmettbarry@users.noreply.github.com>
2022-11-10 13:52:01 -0500

    Staring Nav functinoality

--- RoseBudThorn/App.js
+++ RoseBudThorn/App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
+  HomePage = () => {
+    return(
+      <View style={styles.container}>
+        <Text>This is the Home Page</Text>
+      </View>
+    );
+  }
+
+
+  Profile = () => {
+    return(
+      <View style={styles.container}>
+        <Text>This is the Profile Page</Text>
+      </View>
+    );
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName={HomePage}>
+      <Stack.Screen name="HomePage" component={HomePage} />
+      <Stack.Screen name="Profile" component={Profile} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
@@ -17,4 +42,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
 });
