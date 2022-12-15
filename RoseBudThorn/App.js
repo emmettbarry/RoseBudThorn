ba81ce8a33b96a26cb2701e76bcdb93bce269dc8
kppalmo <kpalmo@ithaca.edu>
2022-12-15 09:06:22 -0500

    textFix

--- RoseBudThorn/App.js
+++ RoseBudThorn/App.js
@@ -128,28 +128,28 @@ export default function App() {
       <View style={{ marginLeft: 30, marginTop: 10 }}>
         <Carousel>
           <View>
-            <Text>Page 1</Text>
+            <Text>Rose</Text>
           </View>
 
           <View>
-            <Text>Page 2</Text>
+            <Text>Bud</Text>
           </View>
           <View>
-            <Text>Page 3</Text>
+            <Text>Thorn</Text>
           </View>
         </Carousel>
       </View>
       <View style={{ marginLeft: 30, marginTop: 10 }}>
       <Carousel>
         <View>
-          <Text>Page 1</Text>
+          <Text>Rose</Text>
         </View>
 
         <View>
-          <Text>Page 2</Text>
+          <Text>Bud</Text>
         </View>
         <View>
-          <Text>Page 3</Text>
+          <Text>Thorn</Text>
         </View>
       </Carousel>
     </View>
